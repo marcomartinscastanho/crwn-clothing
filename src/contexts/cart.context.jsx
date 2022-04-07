0863607a020907babb00e6723cb02d8dee4c5eac
Marco Castanho <marco.castanho@toptal.com>
2022-04-07 23:56:10 +0100

    Cart item designs

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
@@ -16,15 +16,22 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
   const addItemToCart = (product) => setCartItems(addCartItem(cartItems, product));
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  useEffect(() => {
+    const newCardCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+    setCartCount(newCardCount);
+  }, [cartItems]);
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, setCartCount };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
