a9d2f818e51dc34b786fd762f5aff67f7e291923
Marco Castanho <marco.castanho@toptal.com>
2022-04-07 16:33:46 +0100

    Sign Up form - part 1

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,52 @@
+import { Fragment, useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+export const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <Fragment>
+      <h1>Sign Up with your email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          required
+          name="displayName"
+          type="text"
+          value={displayName}
+          onChange={handleChange}
+        />
+
+        <label>Email</label>
+        <input required name="email" type="email" value={email} onChange={handleChange} />
+
+        <label>Password</label>
+        <input required name="password" type="password" value={password} onChange={handleChange} />
+
+        <label>Confirm Password</label>
+        <input
+          required
+          name="confirmPassword"
+          type="password"
+          value={confirmPassword}
+          onChange={handleChange}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import { SignUpForm } from "../../components/sign-up-form/sign-up-form.component";
 
 export const SignIn = () => {
   const logGoogleUser = async () => {
@@ -14,6 +15,7 @@ export const SignIn = () => {
     <Fragment>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </Fragment>
   );
 };
