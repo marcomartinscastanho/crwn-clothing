0fc7817387890d6ee06a00c1376cd0fc631f2e52
Marco Castanho <marco.castanho@toptal.com>
2022-04-07 19:58:45 +0100

    Products context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,11 @@
-import SHOP_DATA from "../../shop-data.json";
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
 
 export const Shop = ({ label, ...otherProps }) => {
+  const { products } = useContext(ProductsContext);
   return (
     <div>
-      {SHOP_DATA.map((product) => (
+      {products.map((product) => (
         <div key={product.id}>
           <h1>{product.name}</h1>
         </div>
