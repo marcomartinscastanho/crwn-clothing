0d5b1499c46281591e9965df9468f26bb1a5e6ac
Marco Castanho <marco.castanho@toptal.com>
2022-04-10 16:10:49 +0100

    User reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,16 +1,63 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from "../utils/firebase/firebase.utils";
 
+/**
+ * HOW REDUCERS WORK:
+ * A reducer is a function that takes an initial state and an action
+ * And returns a new object with the new values (next state) after the action
+ */
+
+// we keep the context
+// the context is the values that we expose, and we want to keep that.
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
+// What changes is how we store and update currentUser
+// we're not using useState for that anymore, we're now using a Reducer
+// Reducer is a function that takes the state and an action
+// Actions are objects with 2 properties: type: string and payload: any
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      // if we receive a action type that we don't recognize
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+/**
+ * WHY adding all this when useState was working perfectly fine ???
+ * This is a very simple example, doesn't seem like we're getting much of adding all of this code
+ * And we aren't.
+ * For simple/small examples like this UserContext, you don't want a reducer.
+ * It's too much boilerplate. Stick with the useState.
+ *
+ * The value of using reducers will prove in more complex cases, like out CartContext.
+ */
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // dispatch is a function that receives an action and calls the reducer with that action
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state; // destructure state to get currentUser
+
+  const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
