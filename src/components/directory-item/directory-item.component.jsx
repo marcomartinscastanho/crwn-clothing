90956e972d95ab15d8ac72a7f3aeca5bba714148
Marco Castanho <marco.castanho@toptal.com>
2022-04-10 13:07:16 +0100

    Last touches

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles";
 
 export const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       {/* imageUrl could be called whatever, it's whatever name we decide to pass a prop into the styled component */}
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
