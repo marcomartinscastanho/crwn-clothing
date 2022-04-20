ac36bf050aac3e81b699ea0ba2f1a34ad70817b6
Marco Castanho <marco.castanho@toptal.com>
2022-04-20 15:29:22 +0100

    React-Redux: creating user reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "./utils/firebase/firebase.utils";
 import { Navigation } from "./routes/navigation/navigation.component";
 import { Authentication } from "./routes/auth/auth.component";
 import { Home } from "./routes/home/home.component";
 import { Shop } from "./routes/shop/shop.component";
 import { Checkout } from "./routes/checkout/checkout.component";
+import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // the user is needed at the entire application
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+
+      dispatch(setCurrentUser(user));
+    });
+
+    // this runs when the component unmounts
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -18,30 +18,6 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-// What changes is how we store and update currentUser
-// we're not using useState for that anymore, we're now using a Reducer
-// Reducer is a function that takes the state and an action
-// Actions are objects with 2 properties: type: string and payload: any
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
-
-const userReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      // if we receive a action type that we don't recognize
-      throw new Error(`Unhandled type ${type} in userReducer`);
-  }
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
-};
-
 /**
  * WHY adding all this when useState was working perfectly fine ???
  * This is a very simple example, doesn't seem like we're getting much of adding all of this code
@@ -52,32 +28,32 @@ const INITIAL_STATE = {
  * The value of using reducers will prove in more complex cases, like out CartContext.
  */
 
-export const UserProvider = ({ children }) => {
-  // dispatch is a function that receives an action and calls the reducer with that action
-  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
-  const { currentUser } = state; // destructure state to get currentUser
+// export const UserProvider = ({ children }) => {
+//   // dispatch is a function that receives an action and calls the reducer with that action
+//   const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+//   const { currentUser } = state; // destructure state to get currentUser
 
-  const setCurrentUser = (user) => dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
-  const value = { currentUser, setCurrentUser };
+//   const setCurrentUser = (user) => dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+//   const value = { currentUser, setCurrentUser };
 
-  useEffect(() => {
-    // when there's a change to auth, the Observer calls the callback function with user as a parameter
-    // passing the setCurrentUser means we update the user
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
+//   useEffect(() => {
+//     // when there's a change to auth, the Observer calls the callback function with user as a parameter
+//     // passing the setCurrentUser means we update the user
+//     const unsubscribe = onAuthStateChangedListener((user) => {
+//       if (user) {
+//         createUserDocumentFromAuth(user);
+//       }
 
-      setCurrentUser(user);
-    });
+//       setCurrentUser(user);
+//     });
 
-    // this runs when the component unmounts
-    return unsubscribe;
-  }, []);
+//     // this runs when the component unmounts
+//     return unsubscribe;
+//   }, []);
 
-  // this provider is allowing any of its child components to have access to
-  // currentUser and setCurrentUser()
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+//   // this provider is allowing any of its child components to have access to
+//   // currentUser and setCurrentUser()
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// };
 
-export default UserProvider;
+// export default UserProvider;
