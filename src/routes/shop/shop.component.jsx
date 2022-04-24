76769588762feb82c91446ca8c90a96a407758b5
Marco Castanho <marco.castanho@toptal.com>
2022-04-24 16:29:32 +0100

    migrate fetchCategories from thunk to saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,16 +4,14 @@ import { useDispatch } from "react-redux";
 // you cannot use a Route component unless its immediate parent is a Route sub-component
 import { CategoriesPreview } from "../categories-preview/categories-preview.component";
 import { Category } from "../category/category.component";
-import { fetchCategoriesAsync } from "../../store/categories/category.action";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 import "./shop.styles.scss";
 
 export const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // here we fetch the categories and set them into the reducer
-    // this is the perferct oportunity to use a thunk
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, [dispatch]);
 
   return (
---
+++ src/store/categories/category.saga.js
@@ -0,0 +1,25 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
+
+export function* fetchCategoriesAsync() {
+  try {
+    // yield call() is like the await, but you don't use await inside a generator
+    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
+    // yield put() is like the dispatch, but you don't dispatch things from a generator
+    yield put(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    yield put(fetchCategoriesFailed(error));
+  }
+}
+
+export function* onFetchCategories() {
+  // when we get a FETCH_CATEGORIES_START, we respond only to the latest one
+  // and it does whatever we put in the second argument
+  yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+}
+
+export function* categoriesSaga() {
+  yield all([call(onFetchCategories)]);
+}
