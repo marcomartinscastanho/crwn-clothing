055329022d344db92a860d069f6ca7a8280646f5
Marco Castanho <marco.castanho@toptal.com>
2022-04-26 17:13:05 +0100

    typing custom middleware

--- src/store/middleware/logger.ts
+++ src/store/middleware/logger.ts
@@ -1,14 +1,17 @@
+import { Middleware } from "redux";
+import { RootState } from "../store";
+
 // currying - evaluating functions with multiple arguments and decomposing them into
 // a sequence of functions with a single argument
 // example
-const curryFunc = (a) => (b, c) => a + b - c;
+const curryFunc = (a: number) => (b: number, c: number) => a + b - c;
 const with3 = curryFunc(3); // a = 3
 with3(2, 4); // 3 + 2 - 4 = 1
 // it's basically a function generator to create re-usable functions
 
 // middlewares are currying functions
 //example
-export const loggerMiddleware = (store) => (next) => (action) => {
+export const loggerMiddleware: Middleware<{}, RootState> = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
   }
