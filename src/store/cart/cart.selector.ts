fbef619963fdd9f4bf3e479f79dc714dcd9f9be7
Marco Castanho <marco.castanho@toptal.com>
2022-04-26 14:34:53 +0100

    typing redux cart

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,6 +1,7 @@
 import { createSelector } from "reselect";
+import { CartState } from "./cart.reducer";
 
-const selectCartReducer = (state) => state.cart;
+const selectCartReducer = (state): CartState => state.cart;
 export const selectCartItems = createSelector([selectCartReducer], (cart) => cart.cartItems);
 export const selectIsCartOpen = createSelector([selectCartReducer], (cart) => cart.isCartOpen);
 export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
