77fda74d1b35e83bc5244e33d2f309c90b51fccc
Marco Castanho <marco.castanho@toptal.com>
2022-04-27 12:21:33 +0100

    typing categories saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
@@ -6,20 +6,20 @@ import { CATEGORIES_ACTION_TYPES } from "./category.types";
 export function* fetchCategoriesAsync() {
   try {
     // yield call() is like the await, but you don't use await inside a generator
-    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
     // yield put() is like the dispatch, but you don't dispatch things from a generator
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
 export function* onFetchCategories() {
   // when we get a FETCH_CATEGORIES_START, we respond only to the latest one
   // and it does whatever we put in the second argument
-  yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+  yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]);
+  yield* all([call(onFetchCategories)]);
 }
